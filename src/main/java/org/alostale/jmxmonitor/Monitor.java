afea0f941a035e8dfe6ef88ac7de2c40b0943fa4
Asier Lostalé <asier.lostale@openbravo.com>
2017-03-03 15:15:21 +0100

    connect to a running jvm

--- src/main/java/org/alostale/jmxmonitor/Monitor.java
+++ src/main/java/org/alostale/jmxmonitor/Monitor.java
@@ -1,5 +1,13 @@
 package org.alostale.jmxmonitor;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.management.MBeanServerConnection;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -7,17 +15,22 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import sun.tools.jconsole.LocalVirtualMachine;
+
 public class Monitor {
+  private int pid;
 
   public static void main(String[] args) throws ParseException {
-    CommandLine cli = getCliOptions(args);
-
-    int pid = Integer.parseInt(cli.getOptionValue("pid"));
-
-    System.out.println(pid);
+    Monitor monitor = new Monitor(args);
+    monitor.execute();
   }
 
-  private static CommandLine getCliOptions(String[] args) {
+  public Monitor(String[] args) {
+    CommandLine params = getCliOptions(args);
+    pid = Integer.parseInt(params.getOptionValue("pid"));
+  }
+
+  private CommandLine getCliOptions(String[] args) {
     Option help = new Option("help", "print this message");
 
     Options ops = new Options();
@@ -31,10 +44,10 @@ public class Monitor {
     } catch (ParseException ingore) {
     }
 
-    Option pid = Option.builder("p").longOpt("pid").argName("pid").type(Integer.class).hasArg()
-        .required().desc("Java process id to monitor").build();
+    Option pidOption = Option.builder("p").longOpt("pid").argName("pid").type(Integer.class)
+        .hasArg().required().desc("Java process id to monitor").build();
 
-    ops.addOption(pid);
+    ops.addOption(pidOption);
 
     if (showHelp) {
       HelpFormatter formatter = new HelpFormatter();
@@ -50,5 +63,39 @@ public class Monitor {
       System.exit(1);
       return null;
     }
+  }
+
+  private void execute() {
+    MBeanServerConnection connection = getBeanServerConnection();
+  }
+
+  private MBeanServerConnection getBeanServerConnection() {
+    Map<Integer, LocalVirtualMachine> allJvms = LocalVirtualMachine.getAllVirtualMachines();
+    MBeanServerConnection connection = null;
+    try {
+      if (!allJvms.containsKey(pid)) {
+        System.err.println("pid " + pid + " not found");
+        System.err.println("  detected jvms are ");
+        for (Entry<Integer, LocalVirtualMachine> jvm : allJvms.entrySet()) {
+          System.err.println("    * " + jvm.getKey() + "\t- " + jvm.getValue().displayName());
+        }
+        System.exit(1);
+      }
+
+      LocalVirtualMachine jvm = LocalVirtualMachine.getLocalVirtualMachine(pid);
+      System.out.println("Connected to " + pid + ": " + jvm.displayName());
+      jvm.startManagementAgent();
+      if (!jvm.isManageable()) {
+        System.err.println("Cannot manage jvm");
+        System.exit(1);
+      }
+
+      JMXServiceURL jmxService = new JMXServiceURL(jvm.connectorAddress());
+      connection = JMXConnectorFactory.connect(jmxService, null).getMBeanServerConnection();
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+    return connection;
   }
 }
